46be2fa8b5468e7afd740be207dd30869547569d
Luke Borsare <lborsare1@gmail.com>
2017-09-12 22:51:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,7 @@ function updatePlaylist(playlist,artistName,songTitle) {
   return playlist
 }
 function removeFromPlaylist(playlist,artistName) {
-  delete playlist.artistName
+  var artistName2 = artistName
+  delete playlist.artistName2
   return playlist
 }
