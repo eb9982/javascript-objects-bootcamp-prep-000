01c3a333615e61ca2b87e78ca06950cb422bb4b8
Luke Borsare <lborsare1@gmail.com>
2017-09-12 22:48:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = {Kanye: 'Famous'}
 
 function updatePlaylist(playlist,artistName,songTitle) {
   const newartist = artistName
-  playlist[newartist] = songTitle
+  playlist.newartist = songTitle
   return playlist
 }
